443eda67802676ea498f9fa357dded05e87cb10c
Stephen Röttger <stephen.roettger@gmail.com>
2014-01-24 15:55:18 -0500

    New main functionality: check if given symbol addresses fit
    The program will now take an uneven number of arguments, first
    the library to check and then at least two tuples consisting of the
    name of a symbol and its address found in a program (e.g. in the GOT).
    The program will then calculate the base address at which the library
    was loaded and confirm that it fits to all given symbol name address
    combinations.
    If successful, the program will return 0 and print the load address
    in hex to stdout. Otherwise, it will return != 0 and print an error
    message to stderr.

--- libdb.py
+++ libdb.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 from elftools.elf.enums import ENUM_ST_INFO_TYPE
@@ -70,19 +72,46 @@ def chunks(l, n):
 if __name__ == "__main__":
   import sys
 
-  libc_elf = MyELFFile(open("/lib/libc.so.6", "r"))
+  if len(sys.argv) < 6 or (len(sys.argv) % 2) != 0:
+    fail("Invalid argument count.", 1)
 
-  if len(sys.argv) > 1:
-    sym_name = sys.argv[1]
-  else:
-    sym_name = "strstr"
-  sym = libc_elf.get_symbol(sym_name)
+  filename = sys.argv[1]
+  func_list = chunks(sys.argv[2:], 2)
 
-  if sym == None:
-    print "Symbol {} not found.".format(sym_name)
-    exit(1)
+  libc_elf = MyELFFile(open(filename, "r"))
 
-  print "{} is {}an IFUNC".format(sym.name, "" if libc_elf.symbol_is_ifunc(sym) else "not ")
+  bin_base = 0
 
-  code = libc_elf.get_segment_at(sym['st_value']).data()
+  for func_name, real_addr in func_list:
+    #try to parse it in decimal first
+    try:
+      real_addr = int(real_addr)
+    except ValueError as e:
+      #try hex instead
+      real_addr = int(real_addr, 16)
+
+    sym = libc_elf.get_symbol(func_name)
+    if sym == None:
+      fail("Symbol {} not found".format(func_name), 2)
+
+    if libc_elf.symbol_is_ifunc(sym):
+      fail("IFUNCs not supported yet.".format(func_name), 3)
+
+    sym_addr = sym['st_value']
+    sym_off = sym_addr - libc_elf.load_addr
+    new_bin_base = real_addr - sym_off
+
+    if new_bin_base == 0:
+      fail("Calculated base is NULL.", 4)
+
+    if new_bin_base < 0:
+      fail("Calculated base is negative.", 5)
+
+    if bin_base != 0 and new_bin_base != bin_base:
+      fail("Offsets don't fit.", 5)
+
+    bin_base = new_bin_base
+
+  print("0x{:x}".format(bin_base))
+  exit(0)
 
